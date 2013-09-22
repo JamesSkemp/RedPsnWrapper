5ca9e36a31e23a751c6d7f861643e7b4abdaf371
James Skemp <strivinglife@gmail.com>
2013-09-22 14:39:14 -0500

    Added functionality to query the API for data about an individual game.

---
+++ RedPsnWrapper/Games.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedPsnWrapper
+{
+	public partial class PsnApi
+	{
+		/// <summary>
+		/// Get an individual game's data.
+		/// </summary>
+		/// <param name="gameId">Id of the game to query.</param>
+		/// <returns>String of data for the game.</returns>
+		public string GetGame(string gameId)
+		{
+			var parameters = string.Format("npwr={0}", gameId);
+			return ApiRequest("getGames", parameters);
+		}
+	}
+}
