00d3553cafd8666dfcc1778241472dfad57aa8a3
James Skemp <strivinglife@gmail.com>
2013-09-22 11:10:56 -0500

    Added helper to make API requests.

---
+++ RedPsnWrapper/ApiRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+
+namespace RedPsnWrapper
+{
+	public partial class PsnApi
+	{
+		/// <summary>
+		/// Helper to make a request to the API.
+		/// </summary>
+		/// <param name="methodPath">Path to the method to call.</param>
+		/// <param name="parameters">Parameters to use.</param>
+		/// <returns>API response.</returns>
+		private string ApiRequest(string methodPath, string parameters)
+		{
+			var requestUrl = string.Format(PsnApi.apiUrl, methodPath, parameters);
+
+			var request = WebRequest.CreateHttp(requestUrl);
+			request.UserAgent = this.UserAgent;
+			request.Timeout = this.Timeout;
+
+			using (var response = request.GetResponse())
+			{
+				StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+				return reader.ReadToEnd();
+			}
+		}
+	}
+}
