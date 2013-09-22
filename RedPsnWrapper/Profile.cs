bc198fc71b63894af9c730ecce8491016a19c7a6
James Skemp <strivinglife@gmail.com>
2013-09-22 14:33:42 -0500

    Added functionality to query API for a user's profile,

---
+++ RedPsnWrapper/Profile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedPsnWrapper
+{
+	public partial class PsnApi
+	{
+		/// <summary>
+		/// Get the profile for an individual user.
+		/// </summary>
+		/// <param name="psnId">Id of the user to query.</param>
+		/// <returns>String of data for the user.</returns>
+		public string GetProfile(string psnId)
+		{
+			var parameters = string.Format("psnid={0}", psnId);
+			return ApiRequest("getGamersProfile", parameters);
+		}
+	}
+}
